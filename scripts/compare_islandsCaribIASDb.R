b8585069cda9ce0eaba43c7ae28c05df2224641f
sacrevert <olipes@ceh.ac.uk>
2018-04-13 17:49:25 +0100

    work on comparing island lists

--- scripts/compare_islandsCaribIASDb.R
+++ scripts/compare_islandsCaribIASDb.R
@@ -9,5 +9,13 @@ iasDbPl_ALL_Std <- read.csv(file = "outputs/IASCaribDb_Plants_longform_FINAL.csv
 CNlist2 <- read.csv(file = "data/CaribbeanOT_links_GATS_and_PA_withISOs_v1.csv", header = T, stringsAsFactors = F)
 # fix a couple of issues with ISO codes
 
+CNlist2[is.na(CNlist2$partner2iso),c(5,30)] # have a look at the ones that have NA, none of importance for the current task
+# drop those lines
+CNlist2_noNAs <- CNlist2[!is.na(CNlist2$partner2iso),]
 
+# just extract the key info for the moment
+CNlist2_noNAs <- CNlist2_noNAs[,c(3,5,28:30)] # focus, partner, source of pathway info, focal ISO, partner ISO
 
+# go through focal islands
+ai <- CNlist2_noNAs[CNlist2_noNAs$reporter=="Anguilla",]
+ai_UniPart <- unique(ai$partner2iso)
